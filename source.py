3b2d8d3c4872f2618d9ec0fa31d4568e1d5078d0
ΑΝΑΣΤΑΣΙΟΣ ΑΓΓΕΛΙΔΗΣ <tassos37000@hotmail.com>
2022-12-18 22:45:48 +0200

    added regularisation

--- source.py
+++ source.py
@@ -3,12 +3,12 @@ import tensorflow as tf
 import numpy as np
 # for visuals
 import tqdm
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
 def filterClasses(x, y):
     """
-    Keeps only classes 5 and 6
+    Keeps only classes 5 and 6, we assume class 5 is 1 and class 6 is 0
     :param x: dataset features
     :param y: dataset labels
     :return: the dataset only with filtered classes
@@ -21,13 +21,13 @@ def filterClasses(x, y):
 
 def reshapeToVector(train):
     """
-    function to reshape images from 28x28 to a 784 vector
+    Reshape images from 28x28 to a 784 vector
     :param train: dataset to reshape
     :return: a numpy array with the reshapes dataset and with type float64
     """
     resh_train = []
-    for i in range(len(train)):
-        resh_train.append(np.reshape(np.array(train[i]), 784).astype("float64"))
+    for j in range(len(train)):
+        resh_train.append(np.reshape(np.array(train[j]), 784).astype("float64"))
     return np.array(resh_train)
 
 
@@ -37,11 +37,11 @@ def rescale(train):
     :param train: dataset
     :return: the rescaled dataset
     """
-    for i in tqdm.tqdm(range(len(train))):
-        for j in range(len(train[i])):
-            if train[i][j] == 0:
+    for k in tqdm.tqdm(range(len(train))):
+        for j in range(len(train[k])):
+            if train[k][j] == 0:
                 continue
-            train[i][j] = train[i][j] / 255.0
+            train[k][j] = train[k][j] / 255.0
 
 
 # a)
@@ -55,11 +55,10 @@ print("Loading: DONE")
 (x_train, y_train) = (x_train[:int(0.8 * len(x_train))], y_train[:int(0.8 * len(y_train))])
 
 # b)
-print("Reshaping...")
 x_train = reshapeToVector(x_train)
 x_val = reshapeToVector(x_val)
 x_test = reshapeToVector(x_test)
-print("Reshaping: DONE\n")
+print("Reshaping: DONE")
 
 print("Rescaling... ")
 rescale(x_train)
@@ -80,49 +79,81 @@ def sigmoid(x):
     return 1.0 / (1.0 + np.exp(-x))
 
 
-def max_likelihood(y_true, y_pred):
+def cost_log_reg(y_true, y_pred, _lambda, theta):
     """
-    Calculates maximum likelihood
+    Calculates the cost for logistic regression
     :param y_true: a numpy array of true values
     :param y_pred: a numpy array of predicted values
-    :return: the maximum likelihood
+    :param _lambda: lambda for regularisation
+    :param theta: a numpy array of weights for regularisation
+    :return: the cost
     """
-    return np.dot(y_true.T, np.log(y_pred)) + np.dot((1 - y_true).T, np.log(1 - y_pred))
+    return np.dot(y_true.T, np.log(y_pred)) + np.dot((1 - y_true).T, np.log(1 - y_pred)) - (_lambda / 2.0) * np.sum(theta ** 2)
 
 
-def logistic_regression(x, y, max_iterations=50, optimizer=0):
-    """Trains logistic regression model using gradient ascent
-    to gain maximum likelihood on the training data
-    Args:
-        x : a numpy array of data
-        y : a numpy array of label
-        max_iterations : number of max iterations
-        optimizer : the optimizer for gradient ascent
-    Returns: array with likelihoods
+def logistic_regression(x, y, max_iterations=50, optimizer=0.0, _lambda=0.0):
     """
-    likelihoods = []
+    Trains logistic regression model using gradient ascent to gain maximum likelihood on the training data
+    :param x: a numpy array of data
+    :param y: a numpy array of label
+    :param max_iterations: number of max iterations (default is 50)
+    :param optimizer: the optimizer for gradient ascent (default is 0.0)
+    :param _lambda: lambda for regularisation (default is 0.0)
+    :returns: array with costs and numpy array of final weights
+    """
+    J_train = []
     theta = np.zeros(x.shape[1])
 
     # Perform gradient ascent
-    for _ in tqdm.tqdm(range(max_iterations)):
+    for _ in range(max_iterations):
         # Output probability value by applying sigmoid on z
         h = sigmoid(np.dot(x, theta))
+        # Calculating max likelihood
+        cur_cost = cost_log_reg(y, h, _lambda, theta)
+        # regularization e)
+        reg = _lambda * theta
         # Calculate the gradient values
-        gradient = np.mean((y - h) * x.T, axis=1)
+        gradient = np.mean((y - h) * x.T, axis=1) - reg
         # Update the weights
         theta = theta + optimizer * gradient
-        # Calculating max likelihood
-        likelihood = max_likelihood(y, h)
-        likelihoods.append(likelihood)
 
-    return likelihoods
+        J_train.append(cur_cost)
+
+    return J_train, theta
 
 
-# Evolution of the cost by iteration
-for opt in [0, 0.1, 0.2, 0.3, 0.4, 0.5]:
-    a = logistic_regression(x_train, y_train, 100, opt)
-    plt.plot(list(range(len(a))), a)
-    plt.title('Evolution of the cost by iteration for optimizer: ' + str(opt))
-    plt.xlabel('Iteration')
-    plt.ylabel('Cost')
-    plt.show()
+# d)
+LR_train, LR_theta = logistic_regression(x_train, y_train, 100, 0.1)
+# LR_train.pop(0)
+# plt.plot(np.arange(len(LR_train)), LR_train)
+# plt.title('Evolution of the cost by iteration for optimizer: ' + str(0.3))
+# plt.xlabel('Iteration')
+# plt.ylabel('Cost')
+# plt.show()
+
+
+def predict(theta, x):
+    p = sigmoid(np.dot(x, theta))
+    prob = p
+    p = p > 0.5 - 1e-6
+
+    return p, prob
+
+
+p_test, prob_test = predict(LR_theta, x_test)
+print('Accuracy of test set', round(np.mean(p_test.astype('int') == y_test) * 100, 3), "%")
+
+print("\nPART E\n")
+list_l2 = np.linspace(10**-4, 10, num=100)
+theta_l2 = []
+accuracy_l2 = []
+
+for i in tqdm.tqdm(range(len(list_l2))):
+    _, LR_theta = logistic_regression(x_train, y_train, 100, 0.1, list_l2[i])
+    theta_l2.append(LR_theta)
+    p_test, prob_test = predict(LR_theta, x_val)
+    accuracy_l2.append(np.mean(p_test.astype('int') == y_val) * 100)
+
+min_val_error = np.where(accuracy_l2 == max(accuracy_l2))
+p_test, prob_test = predict(theta_l2[min_val_error[0][0]], x_test)
+print('Accuracy of test set', round(np.mean(p_test.astype('int') == y_test) * 100, 3), "%")
